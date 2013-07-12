29ada6c2d7c439e0721d8c553aadda34e9ef2e96
Jason Bittel <jason.bittel@gmail.com>
2013-07-12 15:38:00 -0700

    Test valid inputs with the strict parameter

--- test.py
+++ test.py
@@ -37,6 +37,7 @@ class Base32CrockfordTests(unittest.TestCase):
 
     def test_decode_strict(self):
         self.assertRaises(ValueError, base32_crockford.decode, '16j', strict=True)
+        self.assertEqual(base32_crockford.decode('16J', strict=True), 1234)
 
     def test_decode_invalid(self):
         self.assertRaises(ValueError, base32_crockford.decode, 'u')
@@ -47,6 +48,7 @@ class Base32CrockfordTests(unittest.TestCase):
 
     def test_normalize_strict(self):
         self.assertRaises(ValueError, base32_crockford.normalize, 'ix-Lb-Ko', strict=True)
+        self.assertEqual(base32_crockford.normalize('1X1BK0', strict=True), '1X1BK0')
 
     def test_normalize_invalid_unicode(self):
         self.assertRaises(ValueError, base32_crockford.normalize, u'ä')
@@ -54,6 +56,7 @@ class Base32CrockfordTests(unittest.TestCase):
     def test_normalize_invalid_type(self):
         self.assertRaises(TypeError, base32_crockford.normalize, 1234)
         self.assertRaises(TypeError, base32_crockford.normalize, ['16J'])
+
 
 if __name__ == '__main__':
     unittest.main()
