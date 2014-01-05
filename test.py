823ddad1ad7eba56d3531f8cec13af362e442fc3
Jason Bittel <jason.bittel@gmail.com>
2014-01-04 22:47:48 -0800

    Abbreviate import name for tests

--- test.py
+++ test.py
@@ -3,63 +3,63 @@
 
 import unittest
 
-import base32_crockford
+import base32_crockford as b32
 
 
 class Base32CrockfordTests(unittest.TestCase):
 
     def test_encode(self):
-        self.assertEqual(base32_crockford.encode(1234), '16J')
+        self.assertEqual(b32.encode(1234), '16J')
 
     def test_encode_checksum(self):
-        self.assertEqual(base32_crockford.encode(1234, checksum=True), '16JD')
+        self.assertEqual(b32.encode(1234, checksum=True), '16JD')
 
     def test_encode_zero_checksum(self):
-        self.assertEqual(base32_crockford.encode(0, checksum=True), '00')
+        self.assertEqual(b32.encode(0, checksum=True), '00')
 
     def test_encode_negative(self):
-        self.assertRaises(ValueError, base32_crockford.encode, -1)
+        self.assertRaises(ValueError, b32.encode, -1)
 
     def test_encode_float(self):
-        self.assertEqual(base32_crockford.encode(4.2), '4')
+        self.assertEqual(b32.encode(4.2), '4')
 
     def test_encode_split(self):
-        self.assertEqual(base32_crockford.encode(123456, split=2), '3R-J0')
-        self.assertEqual(base32_crockford.encode(123456, split=3), '3RJ-0')
+        self.assertEqual(b32.encode(123456, split=2), '3R-J0')
+        self.assertEqual(b32.encode(123456, split=3), '3RJ-0')
 
     def test_decode(self):
-        self.assertEqual(base32_crockford.decode('16J'), 1234)
+        self.assertEqual(b32.decode('16J'), 1234)
 
     def test_decode_unicode(self):
-        self.assertEqual(base32_crockford.decode(u'16J'), 1234)
+        self.assertEqual(b32.decode(u'16J'), 1234)
 
     def test_decode_checksum(self):
-        self.assertEqual(base32_crockford.decode('16JD', checksum=True), 1234)
+        self.assertEqual(b32.decode('16JD', checksum=True), 1234)
 
     def test_decode_bad_checksum(self):
-        self.assertRaises(ValueError, base32_crockford.decode, '16JE', checksum=True)
+        self.assertRaises(ValueError, b32.decode, '16JE', checksum=True)
 
     def test_decode_strict(self):
-        self.assertRaises(ValueError, base32_crockford.decode, '16j', strict=True)
-        self.assertEqual(base32_crockford.decode('16J', strict=True), 1234)
+        self.assertRaises(ValueError, b32.decode, '16j', strict=True)
+        self.assertEqual(b32.decode('16J', strict=True), 1234)
 
     def test_decode_invalid(self):
-        self.assertRaises(ValueError, base32_crockford.decode, 'u')
-        self.assertRaises(ValueError, base32_crockford.decode, '!')
+        self.assertRaises(ValueError, b32.decode, 'u')
+        self.assertRaises(ValueError, b32.decode, '!')
 
     def test_normalize(self):
-        self.assertEqual(base32_crockford.normalize('ix-Lb-Ko'), '1X1BK0')
+        self.assertEqual(b32.normalize('ix-Lb-Ko'), '1X1BK0')
 
     def test_normalize_strict(self):
-        self.assertRaises(ValueError, base32_crockford.normalize, 'ix-Lb-Ko', strict=True)
-        self.assertEqual(base32_crockford.normalize('1X1BK0', strict=True), '1X1BK0')
+        self.assertRaises(ValueError, b32.normalize, 'ix-Lb-Ko', strict=True)
+        self.assertEqual(b32.normalize('1X1BK0', strict=True), '1X1BK0')
 
     def test_normalize_invalid_unicode(self):
-        self.assertRaises(ValueError, base32_crockford.normalize, u'ä')
+        self.assertRaises(ValueError, b32.normalize, u'ä')
 
     def test_normalize_invalid_type(self):
-        self.assertRaises(TypeError, base32_crockford.normalize, 1234)
-        self.assertRaises(TypeError, base32_crockford.normalize, ['16J'])
+        self.assertRaises(TypeError, b32.normalize, 1234)
+        self.assertRaises(TypeError, b32.normalize, ['16J'])
 
 
 if __name__ == '__main__':
