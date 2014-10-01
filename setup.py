306416523edddb454ebb126c79e86a7dc7f2bd37
Jason Bittel <jason.bittel@gmail.com>
2014-10-01 15:42:58 -0700

    Add Python 2.6 to the classifiers list

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities',
